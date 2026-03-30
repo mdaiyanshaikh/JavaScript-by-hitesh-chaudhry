97fddcc7a6ef55dc5554aa7a8fced792e7ed1aed
Md Aiyan <154621703+mdaiyanshaikh@users.noreply.github.com>
2026-03-30 16:40:08 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,11 +3,11 @@
 // console.log(typeof score); // type is -> number
 
 let score = "33abc";
-console.log(typeof score); // type is string
+// console.log(typeof score); // type is string
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 // note 
 
@@ -18,7 +18,7 @@ console.log(valueInNumber)
 let isLoggedIn = 0 
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // note for boolean
 // 1 =>  true ; 0 => false
@@ -27,11 +27,45 @@ console.log(booleanisLoggedIn);
 
 let someNumber = 123
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // note for String
 // 123 => String
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "maria"
+let str2 = " Khan"
+let str3 = str1 + str2
+// console.log(str3);
+
+// complex
+
+/* console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2")  // 32 */
+
+// note
+// Before string → normal math
+// After string → everything becomes string
+
+let y = 4
+console.log(y++); // 4
+console.log(y); // 5
+
+
+let x = 9 
+console.log(++x); // 10 
+console.log(x); // 10
+
+
+
+
+
 
 
 
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log("2" > 1);
+// console.log(1 > "2");
+
+// console.log(null  > 0);
+// console.log(null == 0);
+// console.log( null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// strict checking
+
+console.log("2" == 2) // => true
+console.log("2" === 2); // => false
+
+// note 
+/* == -> checks only value
+   === -> checks both data type and value (strict checking)
+ */  
+
+
+
+
+
+
+
