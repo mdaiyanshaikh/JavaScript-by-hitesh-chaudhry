339d4c6cab8cd2fed2f011681ffa2b69f44a68cb
Md Aiyan <154621703+mdaiyanshaikh@users.noreply.github.com>
2026-03-30 14:58:10 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = 33;
+
+// console.log(typeof score); // type is -> number
+
+let score = "33abc";
+console.log(typeof score); // type is string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// note 
+
+// 33 => 33 (number)
+// "33abc" => NaN
+// true = > 1; false => 0
+
+let isLoggedIn = 0 
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// note for boolean
+// 1 =>  true ; 0 => false
+// "" => false
+// "any value under this" => true
+
+let someNumber = 123
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// note for String
+// 123 => String
+
+
+
+
+
