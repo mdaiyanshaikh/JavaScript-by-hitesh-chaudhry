ff76d4a67ca92120027dffa945fd6539a3ba1613
Md Aiyan <154621703+mdaiyanshaikh@users.noreply.github.com>
2026-04-01 19:38:58 +0000

    Numbers-And-Maths

---
+++ 01_basics/07_number_And_Maths.js
@@ -0,0 +1,43 @@
+/*const score = 400
+console.log(score); // 400
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString())
+console.log(typeof balance);
+console.log(balance.toFixed(2)) // 100.00
+console.log(balance.toString().length); // 3*/
+
+const num = 123.87663
+// console.log(num.toPrecision(3)); // 124
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In')) // 10,00,000
+
+// ********* *************** MATHS ************************ *************************
+
+/*console.log(Math);
+console.log(Math.abs(-4)); // 4
+console.log(Math.round(4.6)); //5
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.floor(4.9)); // 4
+console.log(Math.min(10,1,0,2)); // 0
+console.log(Math.max(10,5,32,1)); // 32
+*/
+
+console.log(Math.random()) // always gives value between 0 to 1
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
