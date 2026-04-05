e9f21e1120f53de9bc4b4b4023643004b7cfc795
Md Aiyan <154621703+mdaiyanshaikh@users.noreply.github.com>
2026-04-05 17:22:21 +0000

    Json

---
+++ 02_basics/03_objectPart2.js
@@ -0,0 +1,22 @@
+// obejct de-structure
+
+const course = {
+    courseName:"java Script",
+    price:999,
+    courseTeacher:"hitesh sir"
+}
+
+// console.log(course.courseTeacher); // 
+
+// de - structure
+const {courseTeacher} = course
+console.log(courseTeacher);
+
+
+// JSON format
+
+// {
+//     "name":"maria Khan",
+//     "class":10,
+//     "school":"jewish girls school"
+// }
